747665b136bf6800c7c1b7279b4584fd303b463e
supunstr <supunstr@gmail.com>
2021-05-27 23:04:59 +0530

    Use eip association and scacle up web instance

--- Terraform_project.tf
+++ Terraform_project.tf
@@ -3,12 +3,14 @@ provider "aws" {
     region = "us-east-1"
 }
 
+# using default VPC for buld.
 resource "aws_default_vpc" "default" {
   
 }
 
+# creating security prod_web and open port 80 and 443
 resource "aws_security_group" "prod_web" {
-    name = "prod_web"
+    name        = "prod_web"
     description = "Allow http and https ports inbound and everything outbound"
 
 
@@ -39,13 +41,30 @@ resource "aws_security_group" "prod_web" {
     }    
 }
 
+# creating two instances and allocate prod_web security group.
 resource "aws_instance" "prod_web" {
+    count = 2
+
     ami                    = "ami-0b0af3577fe5e3532"
     instance_type          = "t2.micro"
 
     vpc_security_group_ids = [ aws_security_group.prod_web.id ]
 
         tags = {
-        Name : "production"
+            Name : "production"
     } 
+}
+
+# creating elastic IP
+resource "aws_eip" "prod_web_ip" {
+    tags = {
+        Name : "production_IP"
+    } 
+}
+
+# Assinging elastic IP for second instance.
+resource "aws_eip_association" "prod_web" {
+    instance_id   = aws_instance.prod_web[1].id
+    allocation_id = aws_eip.prod_web_ip.id
+
 }
